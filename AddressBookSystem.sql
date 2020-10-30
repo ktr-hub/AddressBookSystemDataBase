d255d6baa583586186d5a16cac1a3e0f2465cdcc
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-10-30 20:02:48 +0530

    [Tirupathi] Add. Create Data Table in DataBase UC2

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -2,3 +2,18 @@
 
 /*UC1-TestCase*/
 CREATE DATABASE AddressBookSystem;
+
+/*UC2-TestCase*/
+CREATE TABLE AddressBookTable 
+(
+ firstName VARCHAR(15),
+ lastName VARCHAR(15),
+ address VARCHAR(15),
+ city VARCHAR(15),
+ state VARCHAR(15),
+ zip INT,
+ phoneNumber VARCHAR(10),
+ email VARCHAR(15)
+);
+
+SELECT * FROM AddressBookTable;
